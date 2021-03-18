de201c6e08d89808cfbf1cf52758e05939e69be8
Ayushi Vishwakarma <cs17mtech11007@iith.ac.in>
2021-03-18 16:07:38 +0530

    fixed the ngStyle issue by declaring the ProductComponentList component in app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 //import { ProductAlertComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    ProductListComponent
     
   ],
   imports: [
