03726aa035a55f5d919a89615dc1bd06f3db2a8b
Ayushi Vishwakarma <cs17mtech11007@iith.ac.in>
2021-03-18 14:29:51 +0530

    created clean slate for event binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,30 +6,12 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
-  isBold: boolean = true;
-  fontSize: number = 30;
-  isItalic: boolean = true;
+export class ProductListComponent{
 
-  setStyle(){
-    let style = {
-      'font-size.px' : this.fontSize,
-      'font-weight' : this.isBold? 'bold': 'normal', 
-      'font-style' : this.isItalic? 'italic': 'normal'
-    }
-    return style;
-  }
-  constructor() { }
 
-  ngOnInit(): void {
+  onClick: void()=>{
+    console.log("button clicked");
   }
 
-  share(){
-    window.alert("Product has been shared!");
-  }
-
-  onNotify(){
-    window.alert("You will be notified when product goes on sale!");
-  }
 
 }
